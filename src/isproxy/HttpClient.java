d2af33a839fb8a44fd1d592f9505f939584f81c4
Jan-Mense <45741815+Jan-Mense@users.noreply.github.com>
2018-12-10 23:17:54 +0100

    gibt einen String mit Header an LogFileWriter
    Hoffe ich habe den richtigen Header bzw nicht zu viele Informationen drin, sieht bei meinem Test so aus:
    Transfer-Encoding: [chunked]; null: [HTTP/1.1 200 OK]; Server: [gws]; P3P: [CP="This is not a P3P policy! See g.co/p3phelp for more info."]; Date: [Mon, 10 Dec 2018 22:05:09 GMT]; Accept-Ranges: [none]; X-Frame-Options: [SAMEORIGIN]; Cache-Control: [private, max-age=0]; Vary: [Accept-Encoding]; Set-Cookie: [NID=150=YPrNvlygWzPLI_ruCMabVdICjhp9XRUDmGq7Ri3a6b_NzeLWCXOFNAOccgnHW9HZYWHDWeDawJDhTGyh6gEVvI9sNIxWsqskUG53u_RzbxP1xMewaG3Y3z1KoKYBinmgWV0Z455MBRG0j6FR9pKUBk7GZwY9oOQjNGJsU9fgjhA; expires=Tue, 11-Jun-2019 22:05:09 GMT; path=/; domain=.google.com; HttpOnly, 1P_JAR=2018-12-10-22; expires=Wed, 09-Jan-2019 22:05:09 GMT; path=/; domain=.google.com]; Expires: [-1]; X-XSS-Protection: [1; mode=block]; Content-Type: [text/html; charset=ISO-8859-1];

--- src/isproxy/HttpClient.java
+++ src/isproxy/HttpClient.java
@@ -1,6 +1,12 @@
+import java.util.*;
+import java.net.*; 
+import java.io.*;
+
 public class HttpClient {
     private final LogFileWriter logfilewriter;
     private final Scanner scanner;
+	private URL url;
+	private String httpHeader;
 
     public HttpClient(LogFileWriter pLogfilewriter, Scanner scanner) {
         this.logfilewriter = pLogfilewriter;
@@ -16,8 +22,24 @@ public class HttpClient {
     }
 
     public void interactiveUrl() {
-    	System.out.println("URL eingeben:")
-    	sc.hasNext();
-    	can parse to URL?Request:return;
+    	System.out.println("URL eingeben:");
+		System.out.print("> ");
+		url = new URL(scanner.nextLine().trim().toLowerCase());
+		urlLookup();
+		LogFileWriter(httpHeader);
     }
+	
+	private void urlLookup(){
+		try {
+			connection = url.openConnection();
+			Map<String, List<String>> map = connection.getHeaderFields();
+            
+			for(Map.Entry<String, List<String>> entry: map.entrySet()){
+				String t = new String(entry.getKey() + ": " + entry.getValue()+"; ");
+				httpHeader += t;
+			}			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
